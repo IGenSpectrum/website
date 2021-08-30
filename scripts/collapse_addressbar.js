fa8c617ed78b529c1b075c0472fe586bb284ced4
QuarkyGirl <nelleanorhall@gmail.com>
2021-08-30 15:26:06 -0700

    add script to collapse addressbar

---
+++ scripts/collapse_addressbar.js
@@ -0,0 +1,28 @@
+var previous = document.body.scrollTop;
+
+let ticking2 = false;
+console.log('hi');
+
+function collapse(){
+  var current = document.body.scrollTop;
+  if (current > previous){
+    window.scrollTo(0,1);
+  }
+  else {
+    window.scrollTo(0,0);
+  }
+  console.log(window.scrollY);
+  previous = current;
+}
+
+if (window.innerWidth <= 1000) {
+  document.body.addEventListener('scroll', function(e){
+    if (!ticking2) {
+      requestAnimationFrame(function(){
+        collapse();
+        ticking2 = false;
+      });
+      ticking2 = true;
+    }
+  });
+}
